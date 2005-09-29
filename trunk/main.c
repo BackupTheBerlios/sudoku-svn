6591d9a4b732d5f7bdca340e0b16e242df4f7176
shadowoflinux <shadowoflinux@a54fac55-de01-0410-88fb-c38832061b93>
2005-09-29 20:38:13 +0000

    Inital Import

---
+++ trunk/main.c
@@ -0,0 +1,797 @@
+#include <stdio.h>
+
+char values[81];
+	char s1values[9];
+	char s2values[9];
+	char s3values[9];
+	char s4values[9];
+	char s5values[9];
+	char s6values[9];
+	char s7values[9];
+	char s8values[9];
+	char s9values[9];
+	char s10values[9];
+	char s11values[9];
+	char s12values[9];
+	char s13values[9];
+	char s14values[9];
+	char s15values[9];
+	char s16values[9];
+	char s17values[9];
+	char s18values[9];
+	char s19values[9];
+	char s20values[9];
+	char s21values[9];
+	char s22values[9];
+	char s23values[9];
+	char s24values[9];
+	char s25values[9];
+	char s26values[9];
+	char s27values[9];
+	char s28values[9];
+	char s29values[9];
+	char s30values[9];
+	char s31values[9];
+	char s32values[9];
+	char s33values[9];
+	char s34values[9];
+	char s35values[9];
+	char s36values[9];
+	char s37values[9];
+	char s38values[9];
+	char s39values[9];
+	char s40values[9];
+	char s41values[9];
+	char s42values[9];
+	char s43values[9];
+	char s44values[9];
+	char s45values[9];
+	char s46values[9];
+	char s47values[9];
+	char s48values[9];
+	char s49values[9];
+	char s50values[9];
+	char s51values[9];
+	char s52values[9];
+	char s53values[9];
+	char s54values[9];
+	char s55values[9];
+	char s56values[9];
+	char s57values[9];
+	char s58values[9];
+	char s59values[9];
+	char s60values[9];
+	char s61values[9];
+	char s62values[9];
+	char s63values[9];
+	char s64values[9];
+	char s65values[9];
+	char s66values[9];
+	char s67values[9];
+	char s68values[9];
+	char s69values[9];
+	char s70values[9];
+	char s71values[9];
+	char s72values[9];
+	char s73values[9];
+	char s74values[9];
+	char s75values[9];
+	char s76values[9];
+	char s77values[9];
+	char s78values[9];
+	char s79values[9];
+	char s80values[9];
+	char s81values[9];
+
+int main()
+{
+
+	int enterloop;
+	int a;
+	int y;
+	int x;
+	    /*char num1;
+    char num2;
+    char num3;
+    char num4;
+    char num5;
+    char num6;
+    char num7;
+    char num8;
+    char num9;
+    char num10;
+    char num11;
+    char num12;
+    char num13;
+    char num14;
+    char num15;
+    char num16;
+    char num17;
+    char num18;
+    char num19;
+    char num20;
+    char num21;
+    char num22;
+    char num23;
+    char num24;
+    char num25;
+    char num26;
+    char num27;
+    char num28;
+    char num29;
+    char num30;
+    char num31;
+    char num32;
+    char num33;
+    char num34;
+    char num35;
+    char num36;
+    char num37;
+    char num38;
+    char num39;
+    char num40;
+    char num41;
+    char num42;
+    char num43;
+    char num44;
+    char num45;
+    char num46;
+    char num47;
+    char num48;
+    char num49;
+    char num50;
+    char num51;
+    char num52;
+    char num53;
+    char num54;
+    char num55;
+    char num56;
+    char num57;
+    char num58;
+    char num59;
+    char num60;
+    char num61;
+    char num62;
+    char num63;
+    char num64;
+    char num65;
+    char num66;
+    char num67;
+    char num68;
+    char num69;
+    char num70;
+    char num71;
+    char num72;
+    char num73;
+    char num74;
+    char num75;
+    char num76;
+    char num77;
+    char num78;
+    char num79;
+    char num80;
+    char num81;*/
+    
+    puts("Please enter the puzzles numbers from left to right, top to bottom.");
+    puts("After entering a number, press return then enter the next number.");
+    puts("Contine until all 81 numbers have been entered.");
+    puts("If a space is blank (has no number) then enter a 0 (thats a zero).");
+    puts("This works for 9*9 puzzles ONLY!\n");
+    puts("Enter 1st value:");
+    scanf("%c",&values[0]);
+    puts("Enter 2nd value:");
+    scanf("%c",&values[1]);
+    puts("Enter 3rd value:");
+    scanf("%c",&values[2]);
+    puts("Enter 4th value:");
+    scanf("%c",&values[3]);
+    puts("Enter 5th value:");
+    scanf("%c",&values[4]);
+    puts("Enter 6th value");
+    scanf("%c",&values[5]);
+    puts("Enter 7th value");
+    scanf("%c",&values[6]);
+    puts("Enter 8th value");
+    scanf("%c",&values[7]);
+    puts("Enter 9th value");
+    scanf("%c",&values[8]);
+    puts("Enter 10th value");
+    scanf("%c",&values[9]);
+    puts("Enter 11th value");
+    scanf("%c",&values[10]);
+    puts("Enter 12th value");
+    scanf("%c",&values[11]);
+    puts("Enter 13th value");
+    scanf("%c",&values[12]);
+    puts("Enter 14th value");
+    scanf("%c",&values[13]);
+    puts("Enter 15th value");
+    scanf("%c",&values[14]);
+    puts("Enter 16th value");
+    scanf("%c",&values[15]);
+    puts("Enter 17th value");
+    scanf("%c",&values[16]);
+    puts("Enter 18th value");
+    scanf("%c",&values[17]);
+    puts("Enter 19th value");
+    scanf("%c",&values[18]);
+    puts("Enter 20th value");
+    scanf("%c",&values[19]);
+    puts("Enter 21st value");
+    scanf("%c",&values[20]);
+    puts("Enter 2nd value");
+    scanf("%c",&values[21]);
+    puts("Enter 23rd value");
+    scanf("%c",&values[22]);
+    puts("Enter 24th value");
+    scanf("%c",&values[23]);
+    puts("Enter 25th value");
+    scanf("%c",&values[24]);
+    puts("Enter 26th value");
+    scanf("%c",&values[25]);
+    puts("Enter 27th value");
+    scanf("%c",&values[26]);
+    puts("Enter 28th value");
+    scanf("%c",&values[27]);
+    puts("Enter 29th value");
+    scanf("%c",&values[28]);
+    puts("Enter 30th value");
+    scanf("%c",&values[29]);
+    puts("Enter 31st value");
+    scanf("%c",&values[30]);
+    puts("Enter 32nd value");
+    scanf("%c",&values[31]);
+    puts("Enter 33rd value");
+    scanf("%c",&values[32]);
+    puts("Enter 34th value");
+    scanf("%c",&values[33]);
+    puts("Enter 35th value");
+    scanf("%c",&values[34]);
+    puts("Enter 36th value");
+    scanf("%c",&values[35]);
+    puts("Enter 37th value");
+    scanf("%c",&values[36]);
+    puts("Enter 38th value");
+    scanf("%c",&values[37]);
+    puts("Enter 39th value");
+    scanf("%c",&values[38]);
+    puts("Enter 40th value");
+    scanf("%c",&values[39]);
+    puts("Enter 41st value");
+    scanf("%c",&values[40]);
+    puts("Enter 42nd value");
+    scanf("%c",&values[41]);
+    puts("Enter 43rd value");
+    scanf("%c",&values[42]);
+    puts("Enter 44th value");
+    scanf("%c",&values[43]);
+    puts("Enter 45th value");
+    scanf("%c",&values[44]);
+    puts("Enter 46th value");
+    scanf("%c",&values[45]);
+    puts("Enter 47th value");
+    scanf("%c",&values[46]);
+    puts("Enter 48th value");
+    scanf("%c",&values[47]);
+    puts("Enter 49th value");
+    scanf("%c",&values[48]);
+    puts("Enter 50th value");
+    scanf("%c",&values[49]);
+    puts("Enter 51st value");
+    scanf("%c",&values[50]);
+    puts("Enter 52nd value");
+    scanf("%c",&values[51]);
+    puts("Enter 53rd value");
+    scanf("%c",&values[52]);
+    puts("Enter 54th value");
+    scanf("%c",&values[53]);
+    puts("Enter 55th value");
+    scanf("%c",&values[54]);
+    puts("Enter 56th value");
+    scanf("%c",&values[55]);
+    puts("Enter 57th value");
+    scanf("%c",&values[56]);
+    puts("Enter 58th value");
+    scanf("%c",&values[57]);
+    puts("Enter 59th value");
+    scanf("%c",&values[58]);
+    puts("Enter 60th value");
+    scanf("%c",&values[59]);
+    puts("Enter 61st value");
+    scanf("%c",&values[60]);
+    puts("Enter 62nd value");
+    scanf("%c",&values[61]);
+    puts("Enter 63rd value");
+    scanf("%c",&values[62]);
+    puts("Enter 64th value");
+    scanf("%c",&values[63]);
+    puts("Enter 65th value");
+    scanf("%c",&values[64]);
+    puts("Enter 66th value");
+    scanf("%c",&values[65]);
+    puts("Enter 67th value");
+    scanf("%c",&values[66]);
+    puts("Enter 68th value");
+    scanf("%c",&values[67]);
+    puts("Enter 69th value");
+    scanf("%c",&values[68]);
+    puts("Enter 70th value");
+    scanf("%c",&values[69]);
+    puts("Enter 71st value");
+    scanf("%c",&values[70]);
+    puts("Enter 72nd value");
+    scanf("%c",&values[71]);
+    puts("Enter 73th value");
+    scanf("%c",&values[72]);
+    puts("Enter 74th value");
+    scanf("%c",&values[73]);
+    puts("Enter 75th value");
+    scanf("%c",&values[74]);
+    puts("Enter 76th value");
+    scanf("%c",&values[75]);
+    puts("Enter 77th value");
+    scanf("%c",&values[76]);
+    puts("Enter 78th value");
+    scanf("%c",&values[77]);
+    puts("Enter 79th value");
+    scanf("%c",&values[78]);
+    puts("Enter 80th value");
+    scanf("%c",&values[79]);
+    puts("Enter 81th value");
+    scanf("%c",&values[80]);
+    
+    /*puts("Enter 1st value:");
+    scanf("%c",&num1);
+    puts("Enter 2nd value:");
+    scanf("%c",&num2);
+    puts("Enter 3rd value:");
+    scanf("%c",&num3);
+    puts("Enter 4th value:");
+    scanf("%c",&num4);
+    puts("Enter 5th value:");
+    scanf("%c",&num5);
+    puts("Enter 6th value:");
+    scanf("%c",&num6);
+    puts("Enter 7th value:");
+    scanf("%c",&num7);
+    puts("Enter 8th value:");
+    scanf("%c",&num8);
+    puts("Enter 9th value:");
+    scanf("%c",&num9);
+    puts("Enter 10th value:");
+    scanf("%c",&num10);
+    puts("Enter 11th value:");
+    scanf("%c",&num11);
+    puts("Enter 12th value:");
+    scanf("%c",&num12);
+    puts("Enter 13th value:");
+    scanf("%c",&num13);
+    puts("Enter 14th value:");
+    scanf("%c",&num14);
+    puts("Enter 15th value:");
+    scanf("%c",&num15);
+    puts("Enter 16th value:");
+    scanf("%c",&num16);
+    puts("Enter 17th value:");
+    scanf("%c",&num17);
+    puts("Enter 18th value:");
+    scanf("%c",&num18);
+    puts("Enter 19th value:");
+    scanf("%c",&num19);
+    puts("Enter 20th value:");
+    scanf("%c",&num20);
+    puts("Enter 21st value:");
+    scanf("%c",&num21);
+    puts("Enter 22nd value:");
+    scanf("%c",&num22);
+    puts("Enter 23rd value:");
+    scanf("%c",&num23);
+    puts("Enter 24th value:");
+    scanf("%c",&num24);
+    puts("Enter 25th value:");
+    scanf("%c",&num25);
+    puts("Enter 26th value:");
+    scanf("%c",&num26);
+    puts("Enter 27th value:");
+    scanf("%c",&num27);
+    puts("Enter 28th value:");
+    scanf("%c",&num28);
+    puts("Enter 29th value:");
+    scanf("%c",&num29);
+    puts("Enter 30th value:");
+    scanf("%c",&num30);
+    puts("Enter 31st value:");
+    scanf("%c",&num31);
+    puts("Enter 32nd value:");
+    scanf("%c",&num32);
+    puts("Enter 33rd value:");
+    scanf("%c",&num33);
+    puts("Enter 34th value:");
+    scanf("%c",&num14);
+    puts("Enter 35th value:");
+    scanf("%c",&num35);
+    puts("Enter 36th value:");
+    scanf("%c",&num36);
+    puts("Enter 37th value:");
+    scanf("%c",&num37);
+    puts("Enter 38th value:");
+    scanf("%c",&num38);
+    puts("Enter 39th value:");
+    scanf("%c",&num39);
+    puts("Enter 40th value:");
+    scanf("%c",&num40);
+    puts("Enter 41st value:");
+    scanf("%c",&num41);
+    puts("Enter 42nd value:");
+    scanf("%c",&num42);
+    puts("Enter 43rd value:");
+    scanf("%c",&num43);
+    puts("Enter 44th value:");
+    scanf("%c",&num44);
+    puts("Enter 45th value:");
+    scanf("%c",&num45);
+    puts("Enter 46th value:");
+    scanf("%c",&num46);
+    puts("Enter 47th value:");
+    scanf("%c",&num47);
+    puts("Enter 48th value:");
+    scanf("%c",&num48);
+    puts("Enter 49th value:");
+    scanf("%c",&num49);
+    puts("Enter 50th value:");
+    scanf("%c",&num50);
+    puts("Enter 51st value:");
+    scanf("%c",&num51);
+    puts("Enter 52nd value:");
+    scanf("%c",&num52);
+    puts("Enter 53rd value:");
+    scanf("%c",&num53);
+    puts("Enter 54th value:");
+    scanf("%c",&num54);
+    puts("Enter 55th value:");
+    scanf("%c",&num55);
+    puts("Enter 56th value:");
+    scanf("%c",&num56);
+    puts("Enter 57th value:");
+    scanf("%c",&num57);
+    puts("Enter 58th value:");
+    scanf("%c",&num58);
+    puts("Enter 59th value:");
+    scanf("%c",&num59);
+    puts("Enter 60th value:");
+    scanf("%c",&num60);
+    puts("Enter 61st value:");
+    scanf("%c",&num61);
+    puts("Enter 62nd value:");
+    scanf("%c",&num62);
+    puts("Enter 63rd value:");
+    scanf("%c",&num63);
+    puts("Enter 64th value:");
+    scanf("%c",&num64);
+    puts("Enter 65th value:");
+    scanf("%c",&num65);
+    puts("Enter 66th value:");
+    scanf("%c",&num66);
+    puts("Enter 67th value:");
+    scanf("%c",&num67);
+    puts("Enter 68th value:");
+    scanf("%c",&num68);
+    puts("Enter 69th value:");
+    scanf("%c",&num69);
+    puts("Enter 70th value:");
+    scanf("%c",&num70);
+    puts("Enter 71st value:");
+    scanf("%c",&num71);
+    puts("Enter 72nd value:");
+    scanf("%c",&num72);
+    puts("Enter 73rd value:");
+    scanf("%c",&num73);
+    puts("Enter 74th value:");
+    scanf("%c",&num74);
+    puts("Enter 75th value:");
+    scanf("%c",&num75);
+    puts("Enter 76th value:");
+    scanf("%c",&num76);
+    puts("Enter 77th value:");
+    scanf("%c",&num77);
+    puts("Enter 78th value:");
+    scanf("%c",&num78);
+    puts("Enter 79th value:");
+    scanf("%c",&num79);
+    puts("Enter 80th value:");
+    scanf("%c",&num80);
+    puts("Enter 81st value:");
+    scanf("%c",&num81);*/
+    for (a=0;a=81;a++)
+	{
+	numelim(a);
+	}
+}
+	numelim(int y)
+	{
+	int x;
+	if (y == 0)
+	  {
+	    if (values[0] != 0)
+	      {
+		x = values[0];
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s7values[x] = 1;
+		s8values[x] = 1;
+		s9values[x] = 1;
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s19values[x] = 1;
+		s20values[x] = 1;
+		s21values[x] = 1;
+		s28values[x] = 1;
+		s37values[x] = 1;
+		s46values[x] = 1;
+		s55values[x] = 1;
+		s64values[x] = 1;
+		s73values[x] = 1;
+		}
+	}
+	if (y == 1)
+	  {
+	    if (values[1] != 0)
+	      {
+		x = values[1];
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s7values[x] = 1;
+		s8values[x] = 1;
+		s9values[x] = 1;
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s19values[x] = 1;
+		s20values[x] = 1;
+		s21values[x] = 1;
+		s29values[x] = 1;
+		s38values[x] = 1;
+		s47values[x] = 1;
+		s56values[x] = 1;
+		s65values[x] = 1;
+		s74values[x] = 1;
+	      }
+	  }
+	if (y ==2)
+	  {
+	    if (values[2] !=0)
+	      {
+		x = values[2];
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s7values[x] = 1;
+		s8values[x] = 1;
+		s9values[x] = 1;
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s19values[x] = 1;
+		s20values[x] = 1;
+		s21values[x] = 1;
+		s30values[x] = 1;
+		s39values[x] = 1;
+		s48values[x] = 1;
+		s57values[x] = 1;
+		s66values[x] = 1;
+		s75values[x] = 1;
+	      }
+	  }
+	if (y == 3)
+	  {
+	    if (values[3] != 0)
+	      {
+		x = values[3];
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s7values[x] = 1;
+		s8values[x] = 1;
+		s9values[x] = 1;
+		s13values[x] = 1;
+		s14values[x] = 1; 
+		s15values[x] = 1;
+		s22values[x] = 1;
+		s23values[x] = 1;
+		s24values[x] = 1;
+		s31values[x] = 1;
+		s40values[x] = 1;
+		s49values[x] = 1;
+		s58values[x] = 1;
+		s67values[x] = 1;
+		s76values[x] = 1;
+	      }
+	  }
+	if (y == 4)
+	  {   
+	    if(values[4] != 0)
+	      {
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s7values[x] = 1;
+		s8values[x] = 1;
+		s9values[x] = 1;
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s13values[x] = 1;
+		s14values[x] = 1;
+		s15values[x] = 1;
+		s22values[x] = 1;
+		s23values[x] = 1;
+		s24values[x] = 1;
+		s32values[x] = 1;
+		s41values[x] = 1;
+		s50values[x] = 1;
+		s59values[x] = 1;
+		s68values[x] = 1;
+		s77values[x] = 1;
+	      }
+	  }
+	if (y == 5)
+	  {
+	    if (values[5] != 0)
+	      {
+		x = values[5];
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s7values[x] = 1;
+		s8values[x] = 1;
+		s9values[x] = 1;
+		s13values[x] = 1;
+		s14values[x] = 1;
+		s15values[x] = 1;
+		s22values[x] = 1;
+		s23values[x] = 1;
+		s24values[x] = 1;
+		s33values[x] = 1;
+		s42values[x] = 1;
+		s51values[x] = 1;
+		s60values[x] = 1;
+		s69values[x] = 1;
+		s78values[x] = 1;
+	      }
+	  }
+	if (y == 6)
+	  {
+	    if (values[6] != 0)
+	      {
+		x = values[6];
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s7values[x] = 1;
+		s8values[x] = 1;
+		s9values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s25values[x] = 1;
+		s26values[x] = 1;
+		s27values[x] = 1;
+		s34values[x] = 1;
+		s43values[x] = 1;
+		s52values[x] = 1;
+		s61values[x] = 1;
+		s70values[x] = 1;
+		s79values[x] = 1;
+	      }
+	  }
+	if (y == 7)
+	  {
+	    if (values[7] != 0)
+	      {
+		x = values[7];
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s7values[x] = 1;
+		s8values[x] = 1;
+		s9values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s25values[x] = 1;
+		s26values[x] = 1;
+		s27values[x] = 1;
+		s35values[x] = 1;
+		s44values[x] = 1;
+		s53values[x] = 1;
+		s62values[x] = 1;
+		s71values[x] = 1;
+		s80values[x] = 1;
+	      }
+	  }
+	if (y == 8)
+	  {
+	    if (values[8] != 0)
+	      {
+		x = values[8];
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s7values[x] = 1;
+		s8values[x] = 1;
+		s9values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s25values[x] = 1;
+		s26values[x] = 1;
+		s27values[x] = 1;
+		s36values[x] = 1;
+		s45values[x] = 1;
+		s54values[x] = 1;
+		s63values[x] = 1;
+		s72values[x] = 1;
+		s81values[x] = 1;
+	      }
+	  }
+	if (y == 9)
+	  {
+	    if (values[9] != 0)
+	      {
+		x = values[9];
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s13values[x] = 1;
+		s14values[x] = 1;
+		s15values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s19values[x] = 1;
+		s20values[x] = 1;
+		s21values[x] = 1;
+		s28values[x] = 1;
+		s37values[x] = 1;
+		s46values[x] = 1;
+		s55values[x] = 1;
+		s64values[x] = 1;
+		s73values[x] = 1;
+	      }
+	  }
+return(0);
+}
+	    
