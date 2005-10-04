c9a4ef977033276cc6877910b6eb9cd07e0b7f76
shadowoflinux <shadowoflinux@a54fac55-de01-0410-88fb-c38832061b93>
2005-10-04 18:43:01 +0000

    converted project to modules and updated makfile, doesn't quite work yet

---
+++ trunk/getvalues.c
@@ -0,0 +1,182 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+ 
+	extern char values[81];
+	
+getvalues()
+{
+	scanf("%c",&values[0]);
+    puts("Enter 2nd value:");
+    scanf("%c",&values[1]);
+    puts("Enter 3rd value:");
+    scanf("%c",&values[2]);
+    puts("Enter 4th value:");
+    scanf("%c",&values[3]);
+    puts("Enter 5th value:");
+    scanf("%c",&values[4]);
+    puts("Enter 6th value");
+    scanf("%c",&values[5]);
+    puts("Enter 7th value");
+    scanf("%c",&values[6]);
+    puts("Enter 8th value");
+    scanf("%c",&values[7]);
+    puts("Enter 9th value");
+    scanf("%c",&values[8]);
+    puts("Enter 10th value");
+    scanf("%c",&values[9]);
+    puts("Enter 11th value");
+    scanf("%c",&values[10]);
+    puts("Enter 12th value");
+    scanf("%c",&values[11]);
+    puts("Enter 13th value");
+    scanf("%c",&values[12]);
+    puts("Enter 14th value");
+    scanf("%c",&values[13]);
+    puts("Enter 15th value");
+    scanf("%c",&values[14]);
+    puts("Enter 16th value");
+    scanf("%c",&values[15]);
+    puts("Enter 17th value");
+    scanf("%c",&values[16]);
+    puts("Enter 18th value");
+    scanf("%c",&values[17]);
+    puts("Enter 19th value");
+    scanf("%c",&values[18]);
+    puts("Enter 20th value");
+    scanf("%c",&values[19]);
+    puts("Enter 21st value");
+    scanf("%c",&values[20]);
+    puts("Enter 2nd value");
+    scanf("%c",&values[21]);
+    puts("Enter 23rd value");
+    scanf("%c",&values[22]);
+    puts("Enter 24th value");
+    scanf("%c",&values[23]);
+    puts("Enter 25th value");
+    scanf("%c",&values[24]);
+    puts("Enter 26th value");
+    scanf("%c",&values[25]);
+    puts("Enter 27th value");
+    scanf("%c",&values[26]);
+    puts("Enter 28th value");
+    scanf("%c",&values[27]);
+    puts("Enter 29th value");
+    scanf("%c",&values[28]);
+    puts("Enter 30th value");
+    scanf("%c",&values[29]);
+    puts("Enter 31st value");
+    scanf("%c",&values[30]);
+    puts("Enter 32nd value");
+    scanf("%c",&values[31]);
+    puts("Enter 33rd value");
+    scanf("%c",&values[32]);
+    puts("Enter 34th value");
+    scanf("%c",&values[33]);
+    puts("Enter 35th value");
+    scanf("%c",&values[34]);
+    puts("Enter 36th value");
+    scanf("%c",&values[35]);
+    puts("Enter 37th value");
+    scanf("%c",&values[36]);
+    puts("Enter 38th value");
+    scanf("%c",&values[37]);
+    puts("Enter 39th value");
+    scanf("%c",&values[38]);
+    puts("Enter 40th value");
+    scanf("%c",&values[39]);
+    puts("Enter 41st value");
+    scanf("%c",&values[40]);
+    puts("Enter 42nd value");
+    scanf("%c",&values[41]);
+    puts("Enter 43rd value");
+    scanf("%c",&values[42]);
+    puts("Enter 44th value");
+    scanf("%c",&values[43]);
+    puts("Enter 45th value");
+    scanf("%c",&values[44]);
+    puts("Enter 46th value");
+    scanf("%c",&values[45]);
+    puts("Enter 47th value");
+    scanf("%c",&values[46]);
+    puts("Enter 48th value");
+    scanf("%c",&values[47]);
+    puts("Enter 49th value");
+    scanf("%c",&values[48]);
+    puts("Enter 50th value");
+    scanf("%c",&values[49]);
+    puts("Enter 51st value");
+    scanf("%c",&values[50]);
+    puts("Enter 52nd value");
+    scanf("%c",&values[51]);
+    puts("Enter 53rd value");
+    scanf("%c",&values[52]);
+    puts("Enter 54th value");
+    scanf("%c",&values[53]);
+    puts("Enter 55th value");
+    scanf("%c",&values[54]);
+    puts("Enter 56th value");
+    scanf("%c",&values[55]);
+    puts("Enter 57th value");
+    scanf("%c",&values[56]);
+    puts("Enter 58th value");
+    scanf("%c",&values[57]);
+    puts("Enter 59th value");
+    scanf("%c",&values[58]);
+    puts("Enter 60th value");
+    scanf("%c",&values[59]);
+    puts("Enter 61st value");
+    scanf("%c",&values[60]);
+    puts("Enter 62nd value");
+    scanf("%c",&values[61]);
+    puts("Enter 63rd value");
+    scanf("%c",&values[62]);
+    puts("Enter 64th value");
+    scanf("%c",&values[63]);
+    puts("Enter 65th value");
+    scanf("%c",&values[64]);
+    puts("Enter 66th value");
+    scanf("%c",&values[65]);
+    puts("Enter 67th value");
+    scanf("%c",&values[66]);
+    puts("Enter 68th value");
+    scanf("%c",&values[67]);
+    puts("Enter 69th value");
+    scanf("%c",&values[68]);
+    puts("Enter 70th value");
+    scanf("%c",&values[69]);
+    puts("Enter 71st value");
+    scanf("%c",&values[70]);
+    puts("Enter 72nd value");
+    scanf("%c",&values[71]);
+    puts("Enter 73th value");
+    scanf("%c",&values[72]);
+    puts("Enter 74th value");
+    scanf("%c",&values[73]);
+    puts("Enter 75th value");
+    scanf("%c",&values[74]);
+    puts("Enter 76th value");
+    scanf("%c",&values[75]);
+    puts("Enter 77th value");
+    scanf("%c",&values[76]);
+    puts("Enter 78th value");
+    scanf("%c",&values[77]);
+    puts("Enter 79th value");
+    scanf("%c",&values[78]);
+    puts("Enter 80th value");
+    scanf("%c",&values[79]);
+    puts("Enter 81th value");
+    scanf("%c",&values[80]);
+}
---
+++ trunk/numelim.c
@@ -0,0 +1,552 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/***********this is producing loads of errors and warnings****************/
+numelim(int y)
+{
+	int x;
+	char s1values[9];
+	char s2values[9];
+	char s3values[9];
+	char s4values[9];
+	char s5values[9];
+	char s6values[9];
+	char s7values[9];
+	char s8values[9];
+	char s9values[9];
+	char s10values[9];
+	char s11values[9];
+	char s12values[9];
+	char s13values[9];
+	char s14values[9];
+	char s15values[9];
+	char s16values[9];
+	char s17values[9];
+	char s18values[9];
+	char s19values[9];
+	char s20values[9];
+	char s21values[9];
+	char s22values[9];
+	char s23values[9];
+	char s24values[9];
+	char s25values[9];
+	char s26values[9];
+	char s27values[9];
+	char s28values[9];
+	char s29values[9];
+	char s30values[9];
+	char s31values[9];
+	char s32values[9];
+	char s33values[9];
+	char s34values[9];
+	char s35values[9];
+	char s36values[9];
+	char s37values[9];
+	char s38values[9];
+	char s39values[9];
+	char s40values[9];
+	char s41values[9];
+	char s42values[9];
+	char s43values[9];
+	char s44values[9];
+	char s45values[9];
+	char s46values[9];
+	char s47values[9];
+	char s48values[9];
+	char s49values[9];
+	char s50values[9];
+	char s51values[9];
+	char s52values[9];
+	char s53values[9];
+	char s54values[9];
+	char s55values[9];
+	char s56values[9];
+	char s57values[9];
+	char s58values[9];
+	char s59values[9];
+	char s60values[9];
+	char s61values[9];
+	char s62values[9];
+	char s63values[9];
+	char s64values[9];
+	char s65values[9];
+	char s66values[9];
+	char s67values[9];
+	char s68values[9];
+	char s69values[9];
+	char s70values[9];
+	char s71values[9];
+	char s72values[9];
+	char s73values[9];
+	char s74values[9];
+	char s75values[9];
+	char s76values[9];
+	char s77values[9];
+	char s78values[9];
+	char s79values[9];
+	char s80values[9];
+	char s81values[9];
+
+	if (y == 0)
+		{
+	    	if (extern values[0] != 0)
+	      	{
+				x = extern values[0];
+				s1values[x] = 1;
+				s2values[x] = 1;
+				s3values[x] = 1;
+				s4values[x] = 1;
+				s5values[x] = 1;
+			s6values[x] = 1;
+			s7values[x] = 1;
+			s8values[x] = 1;
+		s9values[x] = 1;
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s19values[x] = 1;
+		s20values[x] = 1;
+		s21values[x] = 1;
+		s28values[x] = 1;
+		s37values[x] = 1;
+		s46values[x] = 1;
+		s55values[x] = 1;
+		s64values[x] = 1;
+		s73values[x] = 1;
+		}
+		if (y == 1)
+	  	{
+	    if (extern values[1] != 0)
+	      {
+		 x = extern values[1];
+		 s1values[x] = 1;
+		 s2values[x] = 1;
+		 s3values[x] = 1;
+		 s4values[x] = 1;
+		 s5values[x] = 1;
+		 s6values[x] = 1;
+		 s7values[x] = 1;
+		 s8values[x] = 1;
+		 s9values[x] = 1;
+		 s10values[x] = 1;
+		 s11values[x] = 1;
+		 s12values[x] = 1;
+		 s19values[x] = 1;
+		 s20values[x] = 1;
+		 s21values[x] = 1;
+		 s29values[x] = 1;
+		 s38values[x] = 1;
+		 s47values[x] = 1;
+		 s56values[x] = 1;
+		 s65values[x] = 1;
+		 s74values[x] = 1;
+	      }
+	  }
+	if (y ==2)
+	  {
+	    if ( extern values[2] !=0)
+	      {
+		x =  extern values[2];
+		 s1values[x] = 1;
+		 s2values[x] = 1;
+		 s3values[x] = 1;
+		 s4values[x] = 1;
+		 s5values[x] = 1;
+		 s6values[x] = 1;
+		 s7values[x] = 1;
+		 s8values[x] = 1;
+		 s9values[x] = 1;
+		 s10values[x] = 1;
+		 s11values[x] = 1;
+		 s12values[x] = 1;
+		 s19values[x] = 1;
+		 s20values[x] = 1;
+		 s21values[x] = 1;
+		 s30values[x] = 1;
+		 s39values[x] = 1;
+		 s48values[x] = 1;
+		 s57values[x] = 1;
+		 s66values[x] = 1;
+		 s75values[x] = 1;
+	      }
+	  }
+	if (y == 3)
+	  {
+	    if ( extern values[3] != 0)
+	      {
+		x =  extern values[3];
+		 s1values[x] = 1;
+		 s2values[x] = 1;
+		 s3values[x] = 1;
+		 s4values[x] = 1;
+		 s5values[x] = 1;
+		 s6values[x] = 1;
+		 s7values[x] = 1;
+		 s8values[x] = 1;
+		 s9values[x] = 1;
+		 s13values[x] = 1;
+		 s14values[x] = 1; 
+		 s15values[x] = 1;
+		 s22values[x] = 1;
+		 s23values[x] = 1;
+		 s24values[x] = 1;
+		 s31values[x] = 1;
+		 s40values[x] = 1;
+		 s49values[x] = 1;
+		 s58values[x] = 1;
+		 s67values[x] = 1;
+		 s76values[x] = 1;
+	      }
+	  }
+	if (y == 4)
+	  {   
+	    if( extern values[4] != 0)
+	      {
+		 s1values[x] = 1;
+		 s2values[x] = 1;
+		 s3values[x] = 1;
+		 s4values[x] = 1;
+		 s5values[x] = 1;
+		 s6values[x] = 1;
+		 s7values[x] = 1;
+		 s8values[x] = 1;
+		 s9values[x] = 1;
+		 s10values[x] = 1;
+		 s11values[x] = 1;
+		 s12values[x] = 1;
+		 s13values[x] = 1;
+		 s14values[x] = 1;
+		 s15values[x] = 1;
+		 s22values[x] = 1;
+		 s23values[x] = 1;
+		 s24values[x] = 1;
+		 s32values[x] = 1;
+		 s41values[x] = 1;
+		 s50values[x] = 1;
+		 s59values[x] = 1;
+		 s68values[x] = 1;
+		 s77values[x] = 1;
+	      }
+	  }
+	if (y == 5)
+	  {
+	    if ( extern values[5] != 0)
+	      {
+		x =  extern values[5];
+		 s1values[x] = 1;
+		 s2values[x] = 1;
+		 s3values[x] = 1;
+		 s4values[x] = 1;
+		 s5values[x] = 1;
+		 s6values[x] = 1;
+		 s7values[x] = 1;
+		 s8values[x] = 1;
+		 s9values[x] = 1;
+		 s13values[x] = 1;
+		 s14values[x] = 1;
+		 s15values[x] = 1;
+		 s22values[x] = 1;
+		 s23values[x] = 1;
+		 s24values[x] = 1;
+		 s33values[x] = 1;
+		 s42values[x] = 1;
+		 s51values[x] = 1;
+		 s60values[x] = 1;
+		 s69values[x] = 1;
+		 s78values[x] = 1;
+	    }
+	  }
+	if (y == 6)
+	  {
+	    if ( extern values[6] != 0)
+	      {
+		x =  extern values[6];
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s7values[x] = 1;
+		s8values[x] = 1;
+		s9values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s25values[x] = 1;
+		s26values[x] = 1;
+		s27values[x] = 1;
+		s34values[x] = 1;
+		s43values[x] = 1;
+		s52values[x] = 1;
+		s61values[x] = 1;
+		s70values[x] = 1;
+		s79values[x] = 1;
+	      }
+	  }
+	if (y == 7)
+	  {
+	    if (extern values[7] != 0)
+	      {
+		x = extern values[7];
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s7values[x] = 1;
+		s8values[x] = 1;
+		s9values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s25values[x] = 1;
+		s26values[x] = 1;
+		s27values[x] = 1;
+		s35values[x] = 1;
+		s44values[x] = 1;
+		s53values[x] = 1;
+		s62values[x] = 1;
+		s71values[x] = 1;
+		s80values[x] = 1;
+	      }
+	  }
+	if (y == 8)
+	  {
+	    if (extern values[8] != 0)
+	      {
+		x = extern values[8];
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s7values[x] = 1;
+		s8values[x] = 1;
+		s9values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s25values[x] = 1;
+		s26values[x] = 1;
+		s27values[x] = 1;
+		s36values[x] = 1;
+		s45values[x] = 1;
+		s54values[x] = 1;
+		s63values[x] = 1;
+		s72values[x] = 1;
+		s81values[x] = 1;
+	      }
+	  }
+	if (y == 10)
+	  {
+	    if (extern values[10] != 0)
+	      {
+		x = extern values[10];
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s13values[x] = 1;
+		s14values[x] = 1;
+		s15values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s19values[x] = 1;
+		s20values[x] = 1;
+		s21values[x] = 1;
+		s28values[x] = 1;
+		s37values[x] = 1;
+		s46values[x] = 1;
+		s55values[x] = 1;
+		s64values[x] = 1;
+		s73values[x] = 1;
+	      }
+	  }
+	  if (y == 11)
+	  {
+	    if (extern values[11] != 0)
+	      {
+		x = extern values[11];
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s13values[x] = 1;
+		s14values[x] = 1;
+		s15values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s19values[x] = 1;
+		s20values[x] = 1;
+		s21values[x] = 1;
+		s29values[x] = 1;
+		s38values[x] = 1;
+		s47values[x] = 1;
+		s56values[x] = 1;
+		s65values[x] = 1;
+		s74values[x] = 1;
+	      }
+	  }
+	  if (y == 12)
+	  {
+	    if (extern values[12] != 0)
+	      {
+		x = extern values[12];
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s13values[x] = 1;
+		s14values[x] = 1;
+		s15values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s1values[x] = 1;
+		s2values[x] = 1;
+		s3values[x] = 1;
+		s19values[x] = 1;
+		s20values[x] = 1;
+		s21values[x] = 1;
+		s30values[x] = 1;
+		s39values[x] = 1;
+		s48values[x] = 1;
+		s57values[x] = 1;
+		s66values[x] = 1;
+		s75values[x] = 1;
+	      }
+	  }
+	  if (y == 13)
+	  {
+	    if (extern values[13] != 0)
+	      {
+		x = extern values[13];
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s13values[x] = 1;
+		s14values[x] = 1;
+		s15values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s22values[x] = 1;
+		s23values[x] = 1;
+		s24values[x] = 1;
+		s31values[x] = 1;
+		s40values[x] = 1;
+		s49values[x] = 1;
+		s58values[x] = 1;
+		s67values[x] = 1;
+		s76values[x] = 1;
+	      }
+	  }
+	  if (y == 14)
+	  {
+	    if (extern values[14] != 0)
+	      {
+		x = extern values[14];
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s13values[x] = 1;
+		s14values[x] = 1;
+		s15values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s22values[x] = 1;
+		s23values[x] = 1;
+		s24values[x] = 1;
+		s32values[x] = 1;
+		s41values[x] = 1;
+		s50values[x] = 1;
+		s59values[x] = 1;
+		s68values[x] = 1;
+		s77values[x] = 1;
+	      }
+	  }
+	  if (y == 15)
+	  {
+	    if (extern values[15] != 0)
+	      {
+		x = extern values[15];
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s13values[x] = 1;
+		s14values[x] = 1;
+		s15values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s22values[x] = 1;
+		s23values[x] = 1;
+		s24values[x] = 1;
+		s33values[x] = 1;
+		s42values[x] = 1;
+		s51values[x] = 1;
+		s60values[x] = 1;
+		s69values[x] = 1;
+		s78values[x] = 1;
+	      }
+	  }
+	  if (y == 16)
+	  {
+	    if (extern values[16] != 0)
+	      {
+		x = extern values[16];
+		s10values[x] = 1;
+		s11values[x] = 1;
+		s12values[x] = 1;
+		s13values[x] = 1;
+		s14values[x] = 1;
+		s15values[x] = 1;
+		s16values[x] = 1;
+		s17values[x] = 1;
+		s18values[x] = 1;
+		s4values[x] = 1;
+		s5values[x] = 1;
+		s6values[x] = 1;
+		s22values[x] = 1;
+		s23values[x] = 1;
+		s24values[x] = 1;
+		s35values[x] = 1;
+		s44values[x] = 1;
+		s53values[x] = 1;
+		s62values[x] = 1;
+		s71values[x] = 1;
+		s80values[x] = 1;
+			}
+		  }
+	  }
+}
